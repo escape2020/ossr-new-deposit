00cbbee7ff438a2fd1a773f9b6bdf89dd861d895
Thomas Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2021-09-21 15:44:47 +0200

    Create zenodo_prepare_upload.sh

---
+++ zenodo_prepare_upload.sh
@@ -0,0 +1,38 @@
+OUTPUT_DIR=$1
+REPOSITORY_URL=$2
+RELEASE=$3
+
+echo "output dir: $OUTPUT_DIR"
+echo "repository url: $REPOSITORY_URL"
+
+mkdir -p $OUTPUT_DIR
+
+OLDIFS=$IFS
+IFS='/'
+read -a strarr <<< $REPOSITORY_URL
+OWNER_NAME=${strarr[-2]}
+PROJECT_NAME=${strarr[-1]}
+IFS=$OLDIFS
+echo "Deduced project name: $PROJECT_NAME"
+
+if [ -z "$RELEASE" ];
+then
+  echo "looking for a release in the repository"
+  git fetch --prune --tags
+  LAST_RELEASE=`git tag | tail -1`;
+  echo "last release: $LAST_RELEASE"
+  if [ -z "$LAST_RELEASE" ]; then LAST_RELEASE="main"; fi;
+  RELEASE=$LAST_RELEASE
+fi;
+echo "Upload based on $RELEASE";
+
+ARCHIVE="$PROJECT_NAME-$RELEASE.zip"
+wget -O $ARCHIVE $REPOSITORY_URL/archive/$RELEASE.zip
+ls .
+mv $ARCHIVE $OUTPUT_DIR
+
+if test -f codemeta.json; then echo "adding codemeta.json file to $OUTPUT_DIR"; cp codemeta.json $OUTPUT_DIR; else echo "no codemeta.json file to add"; fi;
+
+
+echo "Content of $OUTPUT_DIR :"
+ls -lsh $OUTPUT_DIR
